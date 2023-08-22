e2ae6c945e38e63e58b1eaf8f2ff08cb7e20d2c7
huss2342 <89368091+huss2342@users.noreply.github.com>
2023-08-22 02:32:10 -0500

    start of fsm logic

--- gpu_top.vhd
+++ gpu_top.vhd
@@ -211,6 +211,7 @@ BEGIN
 		if rising_edge(clk) then
 			-- Transfer logic between intermediate signals and memory_controller signals
 			if (ram_initialized = '0' OR ppuctl_done = '1') then
+				--speaking directly to ram--
 				mem_address_a  <= ram_address_a;
 				mem_address_b  <= ram_address_b;
 				mem_data_a     <= ram_data_a;
@@ -225,6 +226,7 @@ BEGIN
 				--mem_wren_a2    <= ram_wren_a2;
 				--mem_wren_b2    <= ram_wren_b2;
 			else
+				--letting the signals from the ppu_controller flow
 				mem_address_a <= int_address_a;
 				mem_address_b <= int_address_b;
 				mem_data_a    <= int_data_a;
@@ -243,119 +245,102 @@ BEGIN
 	end process;
 
 
-	-- INITIALIZING THE RAM
-	RAM_INIT: process(clk, reset) 
-	begin
-		 if reset = '0' then
-			  ram_initialized <= '0'; 
-			  ram_wren_a <= '0';
-			  ram_wren_b <= '0';
-			  init_index <= SECTION_A_START;
-		 elsif rising_edge(clk) AND ram_initialized = '0' then
-			  if init_index <= array_t'LENGTH-1 then --it will iterate til the end of the array
+type fsm_state is (IDLE, INIT_MEMORY, START_PPUCTL, WAIT_PPUCTL_DONE, READ_FROM_MEM, WAIT_FOR_MEMORY, DONE);
+signal current_state, next_state: fsm_state := IDLE;
 
-					-- Writing to Section A
-					ram_address_a <= std_logic_vector(to_unsigned(init_index, ram_address_a'length));
-					ram_data_a <= input_array_a(init_index); -- Value is taken from input_array_a
-					ram_wren_a <= '1';
+process(clk, reset)
+	variable cindex: integer := init_cindex;
+	variable started: boolean := false;
+	variable init_index: integer := SECTION_A_START;
+begin
+	if reset = '0' then
+		int_address_a <= (others => '0');
+		int_address_b <= (others => '0');
+		int_data_a <= (others => '0');
+		int_data_b <= (others => '0');
+		int_wren_a <= '0';
+		int_wren_b <= '0';
 
-					-- Writing to Section B
-					ram_address_b <= std_logic_vector(to_unsigned(init_index + SECTION_B_START, ram_address_b'length));
-					ram_data_b <= input_array_b(init_index); -- Value is taken from input_array_b
-					ram_wren_b <= '1';
+		int_address_a2 <= (others => '0');
+		int_address_b2 <= (others => '0');
+		int_data_a2 <= (others => '0');
+		int_data_b2 <= (others => '0');
+		int_wren_a2 <= '0';
+		int_wren_b2 <= '0';
 
-					init_index <= init_index + 1;
+		ram_address_a <= (others => '0');
+		ram_address_b <= (others => '0');
+		ram_data_a <= (others => '0');
+		ram_data_b <= (others => '0');
+		ram_wren_a <= '0';
+		ram_wren_b <= '0';
 
-			  else
-					ram_wren_a <= '0';
-					ram_wren_b <= '0';
-					ram_initialized <= '1';
-			  end if;
-		 end if;
-	end process RAM_INIT;
+		memory_ready <= '0';
+		current_state <= IDLE;
+		next_state <= IDLE;
 
-	-- STARTING THE PPU_CONTROLLER ONLY WHEN I FINISHED INITIALIZING MEMORY
-	process(clk, reset)
-		 variable started : boolean := false;
-	begin
-		 if reset = '0' then
-			  ppuctl_start <= '0';
-			  started := false;
-		 elsif rising_edge(clk) then
-			  if ram_initialized = '1' AND not started and ppuctl_done = '0'  then
-					ppuctl_opcode <= "00000000";
-					ppuctl_start <= '1';
-					started := true;
-			  elsif ppuctl_done = '1' then
-					gpu_top_done <= '1';
-					ppuctl_start <= '0';
-					started := false;
-			  end if;
-		 end if;
-	end process;
-	
--- Read the Result from Memory (Section C) and Send the Result
-
-	process(clk, reset) 
-		variable cindex : integer := init_cindex; -- cindex to read the result from memory 1024
-	begin
-		if reset = '0' then
-			cindex := init_cindex; 
-			gpu_result_done <= '0';
-			current_state   <= IDLE;
-			next_state      <= IDLE;
-		elsif rising_edge(clk) then
-
-			if ppuctl_done = '1' AND ram_initialized = '1' then
+		
+	elsif rising_edge(clk) then
+		case current_state is
+		
+			when IDLE =>
+				if start = '1' then
+					--the gpu_top receives the start signal
+					next_state <= INIT_MEMORY;
+				end if;
 				
-				case current_state is
-					when IDLE =>
-							if start = '1' then
-							next_state <= READ_FROM_MEM; 
-							else
-
-
-					when READ_FROM_MEM =>
-
-						-- THIS IS READING FROM MEAMORY SEQUENTIALLY, I COULD UPDATE IT TO READ IN PARALLEL LATER
-						-- Calculate the addresses and send them to memory as they are connected
-						
-						--ram_address_a  <= std_logic_vector(to_unsigned(cindex, int_address_a'length));
-						--address_b  <= std_logic_vector(to_unsigned(cindex, address_b'length));
-
-						next_state <= WAIT_FOR_MEMORY;
-
-					when WAIT_FOR_MEMORY =>
-						if memory_ready = '1' then
-							-- Read the result from memory and assign it to result_array
-							result_array(cindex-init_cindex) <= mem_q_a; -- Assuming data_from_memory is the output from memory
-							cindex := cindex + 1;
-							if cindex < array_t'LENGTH-1 then -- the result will be as long as the input array
-								next_state <= READ_FROM_MEM; -- Read the next value
-							else
-								next_state <= DONE;
-							end if;
-						end if;
-						
-					when DONE =>
-						gpu_result_done <= '1'; -- All results have been read
-						cindex := init_cindex;
-						if start = '1' then
-							next_state <= READ_FROM_MEM; -- Transition back to READ_FROM_MEM
-						else
-							next_state <= IDLE; -- Stay in DONE state
-						end if;
-						
-					when others => 
-						gpu_result_done <= '0'; 
-						
-				end case;
+			when INIT_MEMORY =>
+				if init_index > array_t'LENGTH-1 then
+					-- this will iterate and write the input array to memory
+					
+					next_state <= START_PPUCTL;
+				else
+					
+				end if;
 				
-			end if;
-			
-			current_state <= next_state;
-		end if;
-	end process;
+			when START_PPUCTL =>
+				-- Start PPU controller logic
+				...
+				next_state <= WAIT_PPUCTL_DONE;
+				
+			when WAIT_PPUCTL_DONE =>
+				-- Wait for PPU controller done logic
+				...
+				if ppuctl_done = '1' then
+					next_state <= READ_FROM_MEM;
+				end if;
+				
+			when READ_FROM_MEM =>
+				-- Read from memory logic
+				...
+				next_state <= WAIT_FOR_MEMORY;
+				
+			when WAIT_FOR_MEMORY =>
+				-- Wait for memory logic
+				...
+				if cindex < array_t'LENGTH-1 then
+					next_state <= READ_FROM_MEM;
+				else
+					next_state <= DONE;
+				end if;
+				
+			when DONE =>
+				-- Done state logic
+				...
+				if start = '1' then
+					next_state <= READ_FROM_MEM;
+				else
+					next_state <= IDLE;
+				end if;
+				
+			when others =>
+				next_state <= IDLE;
+		end case;
+		
+		current_state <= next_state;
+	end if;
+
+end process;
 
 
 end architecture behavior;
